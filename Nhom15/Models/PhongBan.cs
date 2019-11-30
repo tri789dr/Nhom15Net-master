a622893d0bca5f7c082d6e417a17b0423c990760
Nguyễn Trí <tri789dr1998@gmail.com>
2019-11-30 08:11:36 +0700

    Add project files.

---
+++ Nhom15/Models/PhongBan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhom15.Models
+{
+    public class PhongBan
+    {
+        public int MaPB { get; set; }
+        public string TenPB { get; set; }
+        
+    }
+}
