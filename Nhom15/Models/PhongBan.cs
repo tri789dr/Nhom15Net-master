49f4ddbbfe359c4e36509a65c2cd02cb6d39f6d5
Nguyễn Trí <tri789dr1998@gmail.com>
2019-11-30 08:21:06 +0700

    Phongban

--- Nhom15/Models/PhongBan.cs
+++ Nhom15/Models/PhongBan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace Nhom15.Models
 {
     public class PhongBan
     {
+        [Key]
+        [Display (Name ="Mã Phòng ban")]
         public int MaPB { get; set; }
+        [Display (Name ="Tên Phòng ban")]
         public string TenPB { get; set; }
         
     }
